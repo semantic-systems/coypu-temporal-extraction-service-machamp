787080de391f768067ee35b553be388aa04a4583
Simon <skonline90@posteo.mx>
2023-11-12 11:00:16 +0100

    Deleted unnecessary UIE files

--- uie/dataset_processing/run_data_generation.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding:utf-8 -*-
-
-for data_format in entity relation event absa
-do
-    python uie_convert.py -format spotasoc -config data_config/${data_format} -output ${data_format}
-done
-
-python scripts/data_statistics.py -data converted_data/text2spotasoc/
